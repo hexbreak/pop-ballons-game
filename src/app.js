52f224da21b6766dbbd3d1f3314282ca907da632
hexbreak <theownerproject@gmail.com>
2021-02-03 04:51:25 +0000

    some js worked in

--- src/app.js
+++ src/app.js
@@ -5,6 +5,20 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
-};
+// window.onload = function() {
+//   console.log("Hello Rigo from the console!");
+// };
+
+const container = document.querySelector('.container');
+// let content = "";
+// let balloons = []
+//     'pink', 'black',' red', 'purple'
+// ];
+
+// balloons.forEach(function() {
+//     content = content + 'div class="balloon"></div>';
+// });
+
+// container.innerHTML = content;
+
+// console.log("test!");
